8ef5c861060c4f4c1db1105d5ab09c4068679570
R4H7Lᵏᶦⁿᵍ¿ <114863901+SLYTHERIN12@users.noreply.github.com>
2023-04-11 14:14:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,25 +20,25 @@ global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
 global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
+global.owner = ['5511988883717'] //ur owner number
+global.ownername = "WIZARD" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
-global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
+global.session = "SmlYbUJkUXI=" //session Id
+global.ownernomer = "918968708597" //ur number
+global.lang = "EN" //Select Language "EN" to English "SI" to sinhala 
+global.premium = ['918968708597'] //ur premium number
+global.botname = 'WIZARD ʙᴏᴛ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://www.darknero.ga/' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.themeemoji = "🤡" //ur theme emoji
+global.packname = "Dark WIZARD By" //ur sticker watermark packname
+global.author = "Dark WIZARD Creating By Cyber Yakuza Team" //ur sticker watermark author
+global.wm = "Dark WIZARD Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
